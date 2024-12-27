656d1ac4f25a8db89f0ec327b7dd04e15600eb92
helr <ql25520@163.com>
2024-12-27 16:25:32 +0800

    config(config): Article cover test(chrome)

--- docs/.vuepress/config.ts
+++ docs/.vuepress/config.ts
@@ -28,6 +28,49 @@ export default defineUserConfig({
     docsRepo: '',
     docsDir: 'docs',
 
+    profile: {
+      name: 'helr',
+      description: '描述文字，座右铭/签名',
+      // avatar: '/blogger.png',
+      location: 'shanghai',
+      // organization: '您的组织',
+      circle: true, // 是否为圆形头像
+      layout: 'right', // 个人信息在左侧还是右侧，'left' | 'right'
+    },
+    // 社交链接
+    social: [
+      { icon: 'github', link: 'https://github.com/Helr' },
+      // ... more
+    ],
+    blog:{
+      /**
+       * 通过 glob string 配置包含文件，
+       * 默认读取 源目录中的所有 `.md` 文件，但会排除 `notes` 配置中用于笔记的目录。
+       */
+      include: ['**/*.md'],
+      // 如果希望只将源目录下某个目录下的文章读取为博客文章，比如 `blog` 目录，可以配置为：
+      // include: ['blog/**/*.md'],
+
+      /**
+       * 通过 glob string 配置排除的文件，相对于 源目录
+       */
+      exclude: ['.vuepress/', '**/README.md'],
+
+      // 禁用分页
+      // pagination: false,
+      // 每页显示的文章数量
+      pagination: 15,
+      //  配置 封面图 布局位置
+      // postCover: 'left', // 'left' | 'right' | 'odd-left' | 'odd-right' | 'top'
+      postCover: {
+        // url:'/blog/images/blog/bg-blog.jpg',
+        layout: 'odd-left',
+        ratio: '16:9',
+        width: 300,
+        compact: false
+      }
+    },
+
     // 控制部分自动生成
     autoFrontmatter: {
       permalink: true, // 是否生成永久链接
--- docs/Tool/Browser/Chrome.md
+++ docs/Tool/Browser/Chrome.md
@@ -2,7 +2,8 @@
 title: Chrome
 createTime: 2024/12/23 15:01:15
 permalink: /article/njz3wnox/
-
+cover:
+  url: /blog/images/blog/bg-blog.jpg
 tags:
   - Chrome
   
